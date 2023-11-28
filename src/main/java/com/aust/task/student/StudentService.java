d4747a2ac8ba8028c1dccec1f96795f19590183c
Md. Rafi <alchemist.rafi@gmail.com>
2023-11-28 17:12:04 +0600

    Update

--- src/main/java/com/aust/task/student/StudentService.java
+++ src/main/java/com/aust/task/student/StudentService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 public class StudentService {
 
-
+//
     private List<Student> studentList = new ArrayList<>(Arrays.asList(
             new Student(
                     1L, "Maria", "maria.jamal@gmail.com",
