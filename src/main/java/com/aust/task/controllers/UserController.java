765b99f753129eab7cb27922640497207cd29c66
Md. Rafi <alchemist.rafi@gmail.com>
2023-11-29 17:00:11 +0600

    user controller show, update

--- src/main/java/com/aust/task/controllers/UserController.java
+++ src/main/java/com/aust/task/controllers/UserController.java
@@ -1,4 +1,40 @@
 package com.aust.task.controllers;
 
+import com.aust.task.entity.User;
+import com.aust.task.services.UserService;
+import com.aust.task.student.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/user")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping(path = "/show")
+    public List<User> getUser() {
+        return userService.getUserList();
+    }
+
+    @PostMapping(path = "/add")
+    public List<User> createStudent(@RequestBody User user){
+
+        boolean status = userService.addUser(user);
+
+        if(status)
+            return userService.getUserList();
+        else
+            return null;
+    }
+
+
+
 }
--- src/main/java/com/aust/task/services/UserService.java
+++ src/main/java/com/aust/task/services/UserService.java
@@ -1,8 +1,10 @@
 package com.aust.task.services;
 
 import com.aust.task.entity.User;
+import com.aust.task.student.Student;
 import org.springframework.stereotype.Service;
 
+import javax.jws.soap.SOAPBinding;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,13 +13,52 @@ import java.util.List;
 @Service
 public class UserService {
 
+
+    private enum field{
+        EMAIL,
+        PASSWORD
+    }
+
     private List<User> userList = new ArrayList<>(Arrays.asList(
             new User(1L, "maria", "maria.jamal@gmail.com", "1234"),
             new User(2L, "marzia", "marzia.jamal@gmail.com", "5678")
     ));
 
 
+    public List<User> getUserList(){
+        return userList;
+    }
+
+    public boolean addUser(User user){
+        try{
+            userList.add(user);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
 
 
+    public boolean updateUser(field f, Long uid, String newVal){
+        try {
+            for (User user : userList) {
+
+                if (user.getUid().equals(uid)) {
+
+                    if (f == field.EMAIL) {
+                        user.setEmail(newVal);
+                        return true;
+
+                    } else if (f == field.PASSWORD) {
+                        user.setPassword(newVal);
+                        return true;
+                    }
+                }
+            }
+        } catch (Exception e){
+            return false;
+        }
+        return false;
+    }
 
 }
