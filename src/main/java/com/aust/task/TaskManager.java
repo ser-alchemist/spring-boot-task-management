3eb00a2792ba1b5b7e2e89a132781dc682943691
Md. Rafi <alchemist.rafi@gmail.com>
2023-11-29 09:42:35 +0600

    morning update 2

--- src/main/java/com/aust/task/TaskManager.java
+++ src/main/java/com/aust/task/TaskManager.java
@@ -8,6 +8,6 @@ public class TaskManager {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManager.class, args);
+        System.out.println("Hello World!");
     }
 }
-//commit test
