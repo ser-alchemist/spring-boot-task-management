d292eaf52d34ebdf4f5aa5978071cd440117b4d8
Md. Rafi <alchemist.rafi@gmail.com>
2023-12-27 00:42:37 +0600

    spring security; backend all

---
+++ src/main/java/com/aust/task/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.aust.task.security.services;
+
+import com.aust.task.entity.User;
+import com.aust.task.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByUname(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+    return UserDetailsImpl.build(user);
+  }
+
+}
